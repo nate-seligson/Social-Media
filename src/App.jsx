9c5c25c5d2dc1be28e71d69ba8aa751d81519bf2
nate-seligson <146794258+nate-seligson@users.noreply.github.com>
2024-06-22 18:58:21 -0400

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,63 @@
-import styles from './App.module.css'
-function Info(props){
- return( <h3>
-    <img className = {styles.icon}src = {"./src/assets/" + props.src + ".svg"}></img>
-    <br></br>
-    {props.text}
-    <br></br> 
-    {props.number}
-    </h3>)
+import {Arrow, Heart, Person} from "./svg.jsx"
+import { useState } from "react";
+const iconBasic = "transition-all ease brightness-50 group-hover:brightness-100 fill-green-700"
+function Sidebar(props){
+  const styles = "transition-all ease duration-500 bg-green-950 fixed top-0 bg-fixed w-64 h-screen flex flex-wrap flex-col border-r-4 border-neutral-950 text-center align-middle "
+  return(
+    <div style = {props.open ? {transform: "translateX(0%)"} : {transform: "translateX(-100%)"}} className = {styles}>
+    <h1 className="hover:tracking-widest transition-all ease bg-green-900 w-full h-fit py-10 text-4xl">Natestagram</h1>
+    {props.children}
+    </div>
+  )
+}
+function ProfilePicture(props){
+  return(
+    <img src = "./src/ofo.jpg" className = "transition-all ease hover:rounded rounded-3xl aspect-sqaure border-4 border-green-700 mt-10"></img>
+  )
+}
+function NamePlate(props){
+  return(
+    <div>
+    <h1 className = "text-slate-300 text-2xl hover:tracking-widest transition-all ease">Brian Johnson</h1>
+    <h4 className = "text-neutral-500 text-lg italic hover:tracking-widest transition-all ease ">@brian_johnson99</h4>
+    </div>
+  )
+}
+function Stat(props){
+  return(
+    <div className = "group flex flex-col w-fit">
+      <div className = "stroke-1 transition:all ease w-20 justify-center">
+      {props.icon}
+      </div>
+      <h1 className = "text-lg text-neutral-400 transition-all ease group-hover:tracking-widest group-hover:text-neutral-100"><strong>{props.number}</strong><br />{props.name}</h1>
+    </div>
+
+  )
+}
+function Stats(props){
+ return( 
+ <div className = "flex flex-row w-full justify-center gap-3">
+  <Stat number = "2.2k" name = "loves" icon = {<Heart className = {iconBasic}/>} />
+  <Stat number = "618" name = "followers" icon = {<Person className = {iconBasic} />} />
+ </div>
+)
 }
 function App() {
+  const [open, setOpen] = useState(true);
+  function toggleOpen(){
+    open ? setOpen(false) : setOpen(true);
+  }
   return (
-    <div class = {styles.wrapper}>
-      <div className ={styles.onethirdcontainer}>
-        <h1 className={styles.title}>Natestergram</h1>
-        <img className = {styles.image}src = "src/ofo.jpg"></img>
-        <h1>Brian Johnson</h1>
-        <h6 style = {{marginTop:"-10px", color:"gray"}}>@bjohnson</h6>
-        <div className = {styles.statsdisplay}>
-          <Info text = "followers" src = "person" number = "632"></Info>
-          <Info text = "likes" src = "thumbup" number = "10.2k"></Info>
-          <Info text = "loves" src = "heart" number = "5.5k"></Info>
-        </div><h1 className = {styles.button}>Hello</h1>
-      </div>
-    </div>
+  <aside>
+    <Sidebar open = {open}>
+      <ProfilePicture />
+      <NamePlate />
+      <Stats />
+      <button className = "fixed -right-10 w-10"onClick = {toggleOpen}>
+        <Arrow rotation = "rotate-0" className = "w-full transition-all ease hover:rotate-180"/>
+      </button>
+    </Sidebar>
+  </aside>
   )
 }
 
---
+++ src/svg.jsx
@@ -0,0 +1,27 @@
+import { useState } from "react"
+export function Heart(props){
+    return(
+        <svg className = {props.className} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path fill-rule="evenodd" clip-rule="evenodd" d="M11.9932 5.13581C9.9938 2.7984 6.65975 2.16964 4.15469 4.31001C1.64964 6.45038 1.29697 10.029 3.2642 12.5604C4.89982 14.6651 9.84977 19.1041 11.4721 20.5408C11.6536 20.7016 11.7444 20.7819 11.8502 20.8135C11.9426 20.8411 12.0437 20.8411 12.1361 20.8135C12.2419 20.7819 12.3327 20.7016 12.5142 20.5408C14.1365 19.1041 19.0865 14.6651 20.7221 12.5604C22.6893 10.029 22.3797 6.42787 19.8316 4.31001C17.2835 2.19216 13.9925 2.7984 11.9932 5.13581Z" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"/>
+         </svg>
+    )
+}
+export function Person(props){
+    return(
+        <svg className = {props.className} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M20 21C20 19.6044 20 18.9067 19.8278 18.3389C19.44 17.0605 18.4395 16.06 17.1611 15.6722C16.5933 15.5 15.8956 15.5 14.5 15.5H9.5C8.10444 15.5 7.40665 15.5 6.83886 15.6722C5.56045 16.06 4.56004 17.0605 4.17224 18.3389C4 18.9067 4 19.6044 4 21M16.5 7.5C16.5 9.98528 14.4853 12 12 12C9.51472 12 7.5 9.98528 7.5 7.5C7.5 5.01472 9.51472 3 12 3C14.4853 3 16.5 5.01472 16.5 7.5Z" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"/>
+    </svg>
+    )
+}
+export function Arrow(props){
+    const [clicked, setClicked] = useState(false)
+    rotation = "rotate-0"
+    function clickEvent(){
+        clicked ? setClicked(false): setClicked(true)
+        clicked ? rotation = "rotate-0" : rotation = "rotate-90"
+        console.log(rotation)
+    }
+    return(
+        <svg onClick = {clickEvent} className = {props.className + rotation} viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" ><path d="M12 2a10 10 0 1 0 10 10A10.011 10.011 0 0 0 12 2zm0 18a8 8 0 1 1 8-8 8.009 8.009 0 0 1-8 8z"/><path d="M13.293 7.293 8.586 12l4.707 4.707 1.414-1.414L11.414 12l3.293-3.293-1.414-1.414z"/></svg>
+    )
+}
